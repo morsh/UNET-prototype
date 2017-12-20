d7f7e0f15376ad0a81527ae88a7dc8d169592eb7
morsh <mor.shemesh@gmail.com>
2017-12-20 11:14:24 +0200

    initial commit

---
+++ Assets/Scripts/Client.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class Client : MonoBehaviour {
+
+    private const int MAX_CONNECTIONS = 100;
+
+    private int port = 5701;
+
+    private int hostId;
+    private int webHostId;
+
+    private int reliableChannel;
+    private int unreliableChannel;
+
+    private int connectionId;
+
+    private float connectionTime;
+    private bool isConnected = false;
+    private bool isStarted = false;
+    private byte error;
+
+    private string playerName;
+
+    public void Connect()
+    {
+        // Does the player has a name
+        string playerNameInput = GameObject.Find("NameInput").GetComponent<InputField>().text;
+        if (playerNameInput == "")
+        {
+            Debug.Log("You must enter a name");
+            return;
+        }
+
+        playerName = playerNameInput;
+
+        NetworkTransport.Init();
+        ConnectionConfig connectionConfig = new ConnectionConfig();
+
+        // Client and server need to have the same connection types
+        reliableChannel = connectionConfig.AddChannel(QosType.Reliable);
+        unreliableChannel = connectionConfig.AddChannel(QosType.Unreliable);
+
+        HostTopology topo = new HostTopology(connectionConfig, MAX_CONNECTIONS);
+
+        hostId = NetworkTransport.AddHost(topo, 0);
+        connectionId = NetworkTransport.Connect(hostId, "127.0.0.1", port, 0, out error);
+
+        connectionTime = Time.time;
+        isConnected = true;
+    }
+
+    private void Update()
+    {
+        if (!isConnected) { return; }
+
+        int recHostId;
+        int connectionId;
+        int channelId;
+        byte[] recBuffer = new byte[1024];
+        int bufferSize = 1024;
+        int dataSize;
+        byte error;
+        NetworkEventType recData = NetworkTransport.Receive(out recHostId, out connectionId, out channelId, recBuffer, bufferSize, out dataSize, out error);
+        switch (recData)
+        {
+            case NetworkEventType.Nothing:         //1
+                break;
+            case NetworkEventType.ConnectEvent:    //2
+                break;
+            case NetworkEventType.DataEvent:       //3
+                break;
+            case NetworkEventType.DisconnectEvent: //4
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Server.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Server : MonoBehaviour {
+    private const int MAX_CONNECTIONS = 100;
+
+    private int port = 5701;
+
+    private int hostId;
+    private int webHostId;
+
+    private int reliableChannel;
+    private int unreliableChannel;
+
+    private bool isStarted = false;
+    private byte error;
+
+    private void Start()
+    {
+        NetworkTransport.Init();
+        ConnectionConfig connectionConfig = new ConnectionConfig();
+
+        // Client and server need to have the same connection types
+        reliableChannel = connectionConfig.AddChannel(QosType.Reliable);
+        unreliableChannel = connectionConfig.AddChannel(QosType.Unreliable);
+
+        HostTopology topo = new HostTopology(connectionConfig, MAX_CONNECTIONS);
+
+        hostId = NetworkTransport.AddHost(topo, port, null);
+
+        // Enable browsers to connect to the game
+        webHostId = NetworkTransport.AddWebsocketHost(topo, port, null);
+
+        isStarted = true;
+    }
+
+    private void Update()
+    {
+        if (!isStarted) { return; }
+
+        int recHostId;
+        int connectionId;
+        int channelId;
+        byte[] recBuffer = new byte[1024];
+        int bufferSize = 1024;
+        int dataSize;
+        byte error;
+        NetworkEventType recData = NetworkTransport.Receive(out recHostId, out connectionId, out channelId, recBuffer, bufferSize, out dataSize, out error);
+        switch (recData)
+        {
+            case NetworkEventType.Nothing:         //1
+                break;
+            case NetworkEventType.ConnectEvent:    //2
+                Debug.Log("Player " + connectionId + " has connected");
+                break;
+            case NetworkEventType.DataEvent:       //3
+                string msg = Encoding.Unicode.GetString(recBuffer, 0, dataSize);
+                Debug.Log("Player " + connectionId + " has sent : " + msg);
+                break;
+            case NetworkEventType.DisconnectEvent: //4
+                Debug.Log("Player " + connectionId + " has disconnected");
+                break;
+        }
+    }
+}
